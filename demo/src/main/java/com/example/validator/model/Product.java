d2aeaf689a7daccb1c6e6b3de928409c27e78cd8
Tanishq Deshpande <tanishq252002@gmail.com>
2022-12-13 06:38:04 +0530

    added local crud app

---
+++ demo/src/main/java/com/example/validator/model/Product.java
@@ -0,0 +1,15 @@
+package com.example.validator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
